ad1a9a1f7ed199711e3e73fbaa8815dc215ff7cb
DanMarzo <marzogildan@gmail.com>
2025-02-15 20:50:27 -0300

    Repository para bench tambem, a tabela deve ser criado manualmente

---
+++ Implementation/RepositoryPadrao/Repository.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Domain.Models;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.Design.Serialization;
+
+namespace RepositoryPadrao;
+
+public class Repository
+{
+    private readonly string connection;
+
+    public Repository(string connection)
+    {
+        this.connection = connection;
+    }
+
+    public async Task Insert(PessoaModel pessoa)
+    {
+        using var connection = new SqlConnection(this.connection);
+        await connection.QueryFirstOrDefaultAsync<PessoaModel>("""
+            INSERT INTO Pessoas
+            (Id, Nome, Sobrenome, CPF, Nascimento, Sexo, EmAtividade)
+            OUTPUT INSERTED.*
+            VALUES (@Id, @Nome, @Sobrenome, @CPF, @Nascimento, @Sexo, @EmAtividade);
+            """, pessoa);
+
+    }
+}
