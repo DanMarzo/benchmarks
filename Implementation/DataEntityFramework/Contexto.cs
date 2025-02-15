dbf6d28828f2d5aed831c8611bf5ea431a4a0856
DanMarzo <marzogildan@gmail.com>
2025-02-15 20:49:14 -0300

    projeto para efetuar bench no efcore - (Bagulho simples, nao e querendo refazer a roda sksk)

---
+++ Implementation/DataEntityFramework/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataEntityFramework;
+
+public class Contexto : DbContext
+{
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+
+    public DbSet<PessoaModel> Pessoas { get; set; }
+
+}
