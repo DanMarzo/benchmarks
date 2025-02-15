9b5d89770cd5469a92a70bb3fe4b9b2f5a1cd7a4
DanMarzo <marzogildan@gmail.com>
2025-02-15 20:49:23 -0300

    Dominio

---
+++ Implementation/Domain/Models/PessoaModel.cs
@@ -0,0 +1,15 @@
+﻿using DataApp.Annotations;
+
+namespace Domain.Models;
+
+[TableDB(Nome = "Pessoas")]
+public class PessoaModel
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Nome { get; set; } = string.Empty;
+    public string Sobrenome { get; set; } = string.Empty;
+    public string CPF { get; set; } = string.Empty;
+    public DateTime Nascimento { get; set; }
+    public int? Sexo { get; set; }
+    public bool EmAtividade { get; set; } = true;
+}
